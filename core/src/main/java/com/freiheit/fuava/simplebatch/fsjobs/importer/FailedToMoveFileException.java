189b51d56bce010e3a7f963118b4b50f132270aa
Tim Lessner <tim.lessner@freiheit.com>
2015-07-16 02:58:19 +0200

    File move exceptions more chatty

--- core/src/main/java/com/freiheit/fuava/simplebatch/fsjobs/importer/FailedToMoveFileException.java
+++ core/src/main/java/com/freiheit/fuava/simplebatch/fsjobs/importer/FailedToMoveFileException.java
@@ -11,11 +11,20 @@
 
 package com.freiheit.fuava.simplebatch.fsjobs.importer;
 
+import java.io.File;
+
 /**
  * @author tim.lessner@freiheit.com
  */
 public class FailedToMoveFileException extends Exception {
-    public FailedToMoveFileException( final String absolutePath ) {
-        super( absolutePath );
+    public FailedToMoveFileException( final File toMove, final File moveTo ) {
+
+        super( !toMove.exists()
+            ? toMove.getAbsolutePath() + " does not exist"
+            : !moveTo.exists()
+                ? moveTo.getAbsolutePath() + " does not exist"
+                : !toMove.exists() && !moveTo.exists()
+                    ? "Neither " + toMove.getAbsolutePath() + " exists nor " + moveTo.getAbsolutePath() + " exists"
+                    : "" );
     }
 }
